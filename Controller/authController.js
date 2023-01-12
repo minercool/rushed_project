3efe4f050b8d12f85ce957dc8f674dba86beaddd
minercool <zghidi@gmail.com>
2023-01-12 15:30:33 +0100

    updates

--- Controller/authController.js
+++ Controller/authController.js
@@ -46,3 +46,17 @@ router.post('/signin', async (req, res) => {
     }
 
 })
+
+router.delete('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.status(400).send('Unable to log out')
+        } else {
+          res.send('Logout successful')
+        }
+      });
+    } else {
+      res.end()
+    }
+  })
